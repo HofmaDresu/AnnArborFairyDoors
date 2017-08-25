5a9cfb3bfecc6e0d2aa840b8ac6c2d432e250d67
Matt Hoffman <hofmadresu@gmail.com>
2017-08-25 11:40:18 -0400

    Create prism autofac project

---
+++ AnnArborFairyDoors/AnnArborFairyDoors/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AnnArborFairyDoors.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
